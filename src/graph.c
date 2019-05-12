21d6ce4ec2bb81622e79eeb1a5c4a62fc7484438
Jaskier234 <jaskier234@gmail.com>
2019-05-12 19:29:59 +0200

    poprawione błędy z alokacją pamięci i uzupełniona dokumentacja

--- src/graph.c
+++ src/graph.c
@@ -17,12 +17,48 @@ Graph *newGraph()
 {
     const int INIT_SIZE = 16;
     Graph *graph = calloc(1, sizeof(Graph));
+
+    if(graph == NULL)
+        return NULL;
+
     graph->nodes = calloc(INIT_SIZE, sizeof(Node*));
     graph->tableSize = INIT_SIZE;
     graph->nodeCount = 0;
     graph->ambiguous = newList(NULL);
 
+    if(graph->nodes == NULL || graph->ambiguous == NULL)
+    {
+        deleteGraph(graph);
+        return NULL;
+    }
+
     return graph;
+}
+
+void deleteGraph(Graph *graph)
+{
+    if(graph == NULL)
+        return;
+
+    for(int i=0; i<graph->nodeCount; i++)
+    {
+        free(graph->nodes[i]->id);
+
+        Element *elem = graph->nodes[i]->edges->begin->next;
+        while(elem != graph->nodes[i]->edges->end)
+        {
+            Edge *edge = ((Edge*)elem->value);
+            elem = elem->next;
+
+            removeEdge(graph, edge->v1, edge->v2);
+
+            free(edge);
+        }
+        deleteList(graph->nodes[i]->edges, 0); //todo chyba wystarczy del = true
+    }
+    free(graph->nodes);
+    deleteList(graph->ambiguous, 0);
+    free(graph);
 }
 
 OrientedEdge *newOrientedEdge(Edge *edge, int v)
@@ -36,33 +72,10 @@ OrientedEdge *newOrientedEdge(Edge *edge, int v)
     return orientedEdge;
 }
 
+// chyba nie potrzebne
 void deleteOrientedEdge(OrientedEdge *edge)
 {
     free(edge);
-}
-
-void deleteGraph(Graph *graph)
-{
-    for(int i=0; i<graph->nodeCount; i++)
-    {
-//        free(graph->nodes[i]->label);
-        free(graph->nodes[i]->id);
-
-        Element *elem = graph->nodes[i]->edges->begin->next;
-        while(elem != graph->nodes[i]->edges->end)
-        {
-            Edge *edge = ((Edge*)elem->value);
-            elem = elem->next;
-
-            removeEdge(graph, edge->v1, edge->v2);
-
-            free(edge);
-        }
-        deleteList(graph->nodes[i]->edges, 0);
-    }
-    free(graph->nodes);
-    deleteList(graph->ambiguous, 0);
-    free(graph);
 }
 
 /**
@@ -70,36 +83,30 @@ void deleteGraph(Graph *graph)
  * @return Zwraca wskaźnik na nowo utworzony obiekt Node lub NULL, gdy
  * nie udało się zaalokować pamięci.
  */
-Node *newNode() // TODO poprawić sprawdzanie czy się nie udało
+Node *newNode()
 {
     Node *node = calloc(1, sizeof(Node));
+
     if(node == NULL)
         return NULL;
 
     node->edges = newList(NULL);
     node->id = calloc(1,sizeof(int));
     node->visited = false;
-//    node->label = label;
+
+    if(node->edges == NULL || node->id == NULL)
+    {
+        deleteList(node->edges, false);
+        free(node->id);
+        free(node);
+        return NULL;
+    }
+
     return node;
 }
 
-// tworzy nowy obiekt Node i zwraca jego indeks w grafie. Jeśli nie udało się
-// zaalokować pamięci zwraca NULL
-/**
- * @brief Dodaje wierzchołek do grafu.
- * @param graph Graf, do którego ma zostać dodany wierzchołek.
- * @return Wskaźnik na id wierzchołka lub NULL, gdy nie udało się
- * zaalokować pamięci
- */
 int *addNode(Graph *graph)
 {
-    graph->nodes[graph->nodeCount] = newNode();
-    if(graph->nodes[graph->nodeCount] == NULL)
-        return NULL;
-
-    *graph->nodes[graph->nodeCount]->id = graph->nodeCount;
-    graph->nodeCount++;
-
     if(graph->nodeCount >= graph->tableSize)
     {
         graph->tableSize *= 2;
@@ -107,21 +114,17 @@ int *addNode(Graph *graph)
         if(graph->nodes == NULL)
             return NULL;
     }
+
+    graph->nodes[graph->nodeCount] = newNode();
+    if(graph->nodes[graph->nodeCount] == NULL)
+        return NULL;
+
+    *graph->nodes[graph->nodeCount]->id = graph->nodeCount;
+    graph->nodeCount++;
+
     return graph->nodes[graph->nodeCount-1]->id;
 }
 
-// dodaje krawędź pomiędzy istniejącymi wierzchołkami w grafie i zwraca true jeśli udało się ją stworzyć
-// i false jeśli nie
-/**
- * @brief Dodaje krawędź do grafu pomiędzy wskazanymi wierzchołkami.
- * @param graph Graf, w którym zostanie dodana krawędź
- * @param v1 id wierzchołka do którego zostanie dodana krawędź.
- * @param v2 id wierzchołka do którego zostanie dodana krawędź.
- * @param length Długość krawędzi.
- * @param builtYear Rok budowy lub ostatniego remontu.
- * @return @p true jeśli dodawanie się powiedzie lub @p false, gdy krawędź już jest w grafie,
- * podano nieprawidłowe id wierzchołków lub nie udało się zaalokować pamięci.
- */
 bool addEdge(Graph *graph, int v1, int v2, int length, int builtYear)
 {
     if(getEdge(graph, v1, v2) != NULL) // krawędź już jest w grafie
@@ -161,18 +164,10 @@ int otherNodeId(Edge *edge, int v)
     return edge->v1;
 }
 
-/**
- * @brief Szuka w grafie krawędzi pomiędzy @p v1 i @p v2
- * @param graph Przeszukiwany graf.
- * @param v1 id wierzchołka
- * @param v2 id wierzchołka
- * @return Wskaźnik na krawędź, lub NULL jeśli parametry są niepoprawne lub
- * w grafie nie ma krawędzi pomiędzy @p v1 i @p v2
- */
 Edge *getEdge(Graph *graph, int v1, int v2)
 {
-    if(v1 == v2) // DO DOKUMENTACJI w grafie nie ma pęli
-        return NULL; // dzięki funkcji otherNodeId ten warunek chyba niepotrzebny. Graf może obsługiwać pętle
+    if(v1 == v2)
+        return NULL;
 
     if(v1 >= graph->nodeCount || v2 >= graph->nodeCount) // któregoś z wierzchołków nie ma w grafie
         return NULL;
@@ -189,15 +184,6 @@ Edge *getEdge(Graph *graph, int v1, int v2)
     return NULL; // nie znaleziono
 }
 
-/**
- * @brief Usuwa krawędź z grafu.
- * Nie zwalnia pamięci po usuniętej krawędzi.
- * @param graph Graf, z którego zostanie usunięta krawędź
- * @param v1 id wierzchołka grafu
- * @param v2 id wierzchołka grafu
- * @return @p true jeśli krawędź została poprawnie usunięta lub @p false, gdy
- * nie usunięto krawędzi(np. w grafie jej nie było).
- */
 bool removeEdge(Graph *graph, int v1, int v2)
 {
     if(getEdge(graph, v1, v2) == NULL) // w grafie nie ma takiej krawędzi
@@ -288,13 +274,21 @@ int compare(void *a, void *b)
 }
 
 
-
+// NULL gdy problemy z pamiecią
 List *bestPath(Graph *graph, int v1, int v2)
 {
     PriorityQueue *q = newPriorityQueue(compare);
 
     Path *bestPath = calloc(graph->nodeCount, sizeof(Path));
     Path *secondPath = calloc(graph->nodeCount, sizeof(Path));
+
+    if(bestPath == NULL || secondPath == NULL)
+    {
+        free(bestPath);
+        free(secondPath);
+        deletePriorityQueue(q);
+        return NULL;
+    }
 
     for(int i=0; i<graph->nodeCount; i++)
     {
@@ -303,13 +297,13 @@ List *bestPath(Graph *graph, int v1, int v2)
     }
 
     bestPath[v1] = pathInit(0, maxYear, v1, NULL);
-//    secondPath[v1] = pathInit(0, maxYear, v1, NULL);
+
+    // TODO sprawdzanie czy priorityQueuePush się powiodło
 
     priorityQueuePush(q, &bestPath[v1]);
 
     while(!isEmpty(q))
     {
-//        QueueElement *curr = priorityQueuePop(q);
         Path *n = priorityQueuePop(q);
         int curr = n->nodeId;
 
@@ -317,7 +311,6 @@ List *bestPath(Graph *graph, int v1, int v2)
             continue;
 
         graph->nodes[curr]->visited = true;
-//        Element *edges = graph->nodes[curr]->edges->begin->next;
 
         foreach(it, graph->nodes[curr]->edges)
         {
@@ -353,16 +346,26 @@ List *bestPath(Graph *graph, int v1, int v2)
         }
     }
 
-    for(int i=0; i<graph->nodeCount; i++)
+    for(int i=0; i<graph->nodeCount; i++) {
         graph->nodes[i]->visited = false;
+    }
 
+    deletePriorityQueue(q);
 
     // TODO zwalnianie
     if(secondPath[v2].dist == bestPath[v2].dist && secondPath[v2].year == bestPath[v2].year && bestPath[v2].dist != INF)
+    {
+        free(bestPath);
+        free(secondPath);
         return graph->ambiguous;
+    }
 
     if(bestPath[v2].parent == NULL) // brak ścieżki
+    {
+        free(bestPath);
+        free(secondPath);
         return NULL;
+    }
 
 //    if(bestPath[v2].pathCount != 1 || bestPath[v2].parent == NULL)
 //    {
@@ -383,11 +386,12 @@ List *bestPath(Graph *graph, int v1, int v2)
         v2 = bestPath[v2].parent->v;
     }
 
-    deletePriorityQueue(q);
     for(int i=0; i<graph->nodeCount; i++)
         if(bestPath[i].parent != NULL && bestPath[i].parent->isInPath == false)
             free(bestPath[i].parent);
+
     free(bestPath);
+    free(secondPath);
 
     return path;
 }
--- src/graph.h
+++ src/graph.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * Interfejs grafu. //todo
+ */
+
 #ifndef DROGI_GRAPH_H
 #define DROGI_GRAPH_H
 
@@ -15,8 +20,8 @@ typedef struct Node
 {
     List *edges; ///< Lista krawędzi  wychodzących z wierzchołka
     int *id; ///< Numer wierzchołka.
-    bool visited; // TODO może przenieść to do dijkstry// chyba jednak nie
-//    char *label;
+    bool visited; ///< Informacja o tym, czy wierzchołek został już odwiedzony
+    ///< podczas wyszukiwania ścieżki
 } Node;
 
 /**
@@ -24,9 +29,9 @@ typedef struct Node
  */
 typedef struct Egde
 {
-    int v1, v2;
-    int length;
-    int builtYear;
+    int v1, v2; ///< Indeksy wierzchołków
+    int length; ///< Długość krawędzi
+    int builtYear; ///< Rok budowy lub ostatniego remontu
 } Edge;
 
 /**
@@ -40,31 +45,82 @@ typedef struct Graph
     List *ambiguous; ///< do bestPath
 } Graph;
 
+/**
+ * Struktura przechowująca krawędź wraz z wyróżnionym wierzchołkiem
+ */
 typedef struct OrientedEdge
 {
-    int v;
-    Edge *edge;
-    bool isInPath;
+    int v; ///< Indeks wierzchołka, który jest wyróżniony
+    Edge *edge; ///< Wskaźnik na krawędź
+    bool isInPath; ///< Zmienna mówiąca, czy obiekt powinien zostać usunięty
+    ///< po zakończeniu wyszukiwania ścieżki
 } OrientedEdge;
 
+/**
+ * @brief Tworzy nowy graf.
+ * @return Wskaźnik na graf lub NULL w przypadkum gdy nie udało się
+ * zaalokować pamięci
+ */
 Graph *newGraph();
 
+/**
+ * @brief Zwalnia pamięć po grafie.
+ * @param graph Wskaźnil na graf, który ma zostać usunięty. Gdy równe NULL
+ * funkcja nic nie robi.
+ */
 void deleteGraph(Graph *graph);
 
-void deleteOrientedEdge(OrientedEdge *edge);
+//void deleteOrientedEdge(OrientedEdge *edge);
 
+/**
+ * @brief Dodaje wierzchołek do grafu.
+ * @param graph Graf, do którego ma zostać dodany wierzchołek.
+ * @return Wskaźnik na id wierzchołka lub NULL, gdy nie udało się
+ * zaalokować pamięci.
+ */
 int *addNode(Graph *graph);
 
+/**
+ * @brief Dodaje krawędź do grafu pomiędzy wskazanymi wierzchołkami.
+ * @param graph Graf, w którym zostanie dodana krawędź
+ * @param v1 id wierzchołka do którego zostanie dodana krawędź.
+ * @param v2 id wierzchołka do którego zostanie dodana krawędź.
+ * @param length Długość krawędzi.
+ * @param builtYear Rok budowy lub ostatniego remontu.
+ * @return @p true jeśli dodawanie się powiedzie lub @p false, gdy krawędź już jest w grafie,
+ * podano nieprawidłowe id wierzchołków lub nie udało się zaalokować pamięci.
+ */
 bool addEdge(Graph *graph, int v1, int v2, int length, int builtYear);
 
+/**
+ * @brief Szuka w grafie krawędzi pomiędzy @p v1 i @p v2
+ * @param graph Przeszukiwany graf.
+ * @param v1 id wierzchołka
+ * @param v2 id wierzchołka
+ * @return Wskaźnik na krawędź, lub NULL jeśli parametry są niepoprawne lub
+ * w grafie nie ma krawędzi pomiędzy @p v1 i @p v2
+ */
 Edge *getEdge(Graph *graph, int v1, int v2);
 
+/**
+ * @brief Usuwa krawędź z grafu.
+ * Nie zwalnia pamięci po usuniętej krawędzi.
+ * @param graph Graf, z którego zostanie usunięta krawędź
+ * @param v1 id wierzchołka grafu
+ * @param v2 id wierzchołka grafu
+ * @return @p true jeśli krawędź została poprawnie usunięta lub @p false, gdy
+ * nie usunięto krawędzi(np. w grafie jej nie było).
+ */
 bool removeEdge(Graph *graph, int v1, int v2);
 
 List *bestPath(Graph *graph, int v1, int v2);
 
 int min(int a, int b);
 
+/**
+ * @brief Funkcja wypisująca listę sąsiedztwa grafu na standardowe wyjście.
+ * @param graph Wsgaźnik na graf, który ma zostać wypisany.
+ */
 void printGraph(Graph *graph);
 
 #endif //DROGI_GRAPH_H
